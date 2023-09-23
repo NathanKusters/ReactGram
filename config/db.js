236e3727db751ba654aa8e786f433ae5168a0220
Nathan Küsters <nathankusterss@gmail.com>
2023-09-22 21:04:35 -0300

    fazendo a conexão com o banco de dados

--- config/db.js
+++ config/db.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-// conection
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.j9wmg8p.mongodb.net/?retryWrites=true&w=majority`
+        );
+
+        console.log("Conectou ao banco!");
+
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = conn;
+
